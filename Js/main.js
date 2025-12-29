5888f4a431fb97038bc692e0164ed646d4f7734f
wongsakornj-creat <wongsakorn.j@ku.th>
2025-12-29 16:56:50 +0700

    js file

---
+++ Js/main.js
@@ -0,0 +1,94 @@
+//
+let firstName = "bank"
+const idcard ='123'
+
+let age = 30;
+let height = 5.9;
+
+let isStudent = false
+
+firstName= 'test'
+
+console.log('My name id',firstName,'and i am',age,'year old')
+
+
+// + - * /
+let number1 = 10;
+let number2 = 5;
+
+let result = number1 + number2;
+console.log("ผลคูณ",result);
+
+
+
+let number1 = 10;
+let number2 = 20;
+
+let conditionl = number1 != number2
+console.log('result of condition is:',conditionl)
+
+
+// if,else if ,else
+let number1 = 22;
+let number2 = 22;
+
+if(number1 != number2){
+    console.log("this is");
+}else if (number1 == number2){
+    console.log("this else if")
+}else{
+    console.log("this else")
+}
+
+
+
+let score = prompt("tell your score")
+console.log("our get score ="+score)
+
+if(score>= 80){
+    console.log("Grade A")
+}else if(score>=70){
+    console.log("Grade B")
+}else if (score>=60){
+    console.log("Grade C")
+}else if(score>=50){
+    console.log("Grade D")
+}else{
+    console.log("Grade F")
+}
+
+
+let number1 = 5
+let number2 = 8
+
+let condition = !(number1  >= 3 || number2 >=10)
+console.log("condition",condition)
+
+let age = 30
+let gender = "men"
+
+if(age = 20 && gender ){
+    console.log(" you older than 20")
+}
+
+
+
+let number = 20 
+
+if (number % 2 != 0){
+    console.log("even number)
+}
+
+
+//loop
+let counter = -5
+while(counter < 10){
+    console.log("Hello world from while loop")
+    counter += 1
+}
+
+for(let i=0;i<=10;i++){
+    console.log("Hello world from for loop")
+}
+
+
