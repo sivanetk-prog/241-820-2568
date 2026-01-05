abee807869d6ad291e09f1074d8a3744bb47be29
sivanetk-prog <sivanetk.k@ku.th>
2026-01-05 17:00:24 +0700

    add js

--- Js/main.js
+++ Js/main.js
@@ -1,94 +1,168 @@
-/*
-let firstName = "bank"
-const idcard ='123'
-
-let age = 30;
-let height = 5.9;
-
-let isStudent = false
-
-firstName= 'test'
-
-console.log('My name id',firstName,'and i am',age,'year old')
 
 
-// + - * /
-let number1 = 10;
-let number2 = 5;
+/*array
+let a = 10 
+let b = 20
+let c = 50
+console.log('a:',a,'b:',b,'c:',c)
 
-let result = number1 + number2;
-console.log("ผลคูณ",result);
+let ages =[10,20,50]// array 3 time
+console.log('ages',ages)
+console.log('ages[1]',ages[1])
 
+//แทนที่
+ages=[15,25]
+console.log('ages lits',ages)
+//ต่อ
+ages.push[35]
+console.log('ages after push',ages)
 
-
-let number1 = 10;
-let number2 = 20;
-
-let conditionl = number1 != number2
-console.log('result of condition is:',conditionl)
-
-
-// if,else if ,else
-let number1 = 22;
-let number2 = 22;
-
-if(number1 != number2){
-    console.log("this is");
-}else if (number1 == number2){
-    console.log("this else if")
-}else{
-    console.log("this else")
-}
-
-
-
-let score = prompt("tell your score")
-console.log("our get score ="+score)
-
-if(score>= 80){
-    console.log("Grade A")
-}else if(score>=70){
-    console.log("Grade B")
-}else if (score>=60){
-    console.log("Grade C")
-}else if(score>=50){
-    console.log("Grade D")
-}else{
-    console.log("Grade F")
-}
-
-
-let number1 = 5
-let number2 = 8
-
-let condition = !(number1  >= 3 || number2 >=10)
-console.log("condition",condition)
-
-let age = 30
-let gender = "men"
-
-if(age = 20 && gender ){
-    console.log(" you older than 20")
-}
-
-
-
-let number = 20 
-
-if (number % 2 != 0){
-    console.log("even number)
-}
-
-
-//loop
-let counter = -5
-while(counter < 10){
-    console.log("Hello world from while loop")
-    counter += 1
-}
-
-for(let i=0;i<=10;i++){
-    console.log("Hello world from for loop")
-}
-
+//
+ages.pop()
+console.log('ages after pop',ages)
 */
+
+/*
+//
+let ages = [24,30,35,40,45]
+console.log('Ages',ages)
+
+let fruit=['apple','banana','cherry']
+console.log('Fruit',fruit)
+fruit.push('mango')
+console.log('Fruit after push',fruit)
+console.log('first fruit',fruit.length)
+
+for(let i =0;i < fruit.length;i++){
+    console.log('Fruit at index s($)',fruit[i])
+}
+*/
+
+/*
+//
+let student1 = [{
+    age:30,
+    name:"john",
+    grade:'A'
+},{
+    age:30,
+    name:"beee",
+    grade:'A'
+}]
+console.log(student1)
+console.log('name',student1.name)
+console.log('age',student1.age)
+console.log('grade',student1.grade)
+
+for(let i =0;i< student1.length;i++){
+    console.log("student"+[i+1]+":")
+console.log('name'+student1[i].name)
+console.log('age',student1[i].age)
+console.log('grade',student1[i].grade)
+
+}
+
+student1.push({
+    age:28,
+    name:"mike",
+    grade:'C'
+})
+console.log("Added",student1[student1.length-1])
+
+student1.pop()
+console.log("Remove",student1)
+*/
+
+/*
+//function
+let score1 = 10 
+let score2 = 80
+//ประกาศฟังก์ชัน
+function calculate_Grade(score){
+if(score >= 80 ){
+    grade='A'
+}else if(score >= 70){
+    grade='B'
+}else if(score >= 60){
+    grade='C'
+}else if(score >= 50){
+    grade='D'
+}else{
+    grade='F'
+}
+return grade
+}
+//เรียกใช้ฟังก์ชัน calculate_Grade เพื่อตรวจสอบเกรด
+let grade1 = calculate_Grade(score1)
+let grade2 = calculate_Grade(score2)
+console.log('Score1:',score1,'Grade1:',grade1)
+console.log('Score2:',score2,'Grade2:',grade2)
+*/   
+
+//array 
+/*
+let score = [95,67,80,45,72]
+for(let i =0;i < score.length;i++){
+    console.log('`Score ${i+1}`',score[i])
+}  
+score= score.map((s) => {
+    return s - 10
+})
+
+score.forEach(($ => {
+    console.log('Score:',$)
+}))
+*/
+/*
+let score = [95,67,80,45,72]
+let ner5score = []
+
+for(let i =0;i < score.length;i++){
+    console.log(score[i])
+    if(score[i] >= 60){
+        ner5score.push(score[i])
+    }
+}
+ner5score.forEach((ns) => {
+    console.log('New Score:'+ ns)
+ }) 
+*/
+
+/*
+let score = [90,80,70,60,50]
+let newscore = []
+
+for(let i =0;i < score.length;i++){
+    console.log(score[i])
+}
+let ner5score = score.filter(function(score){
+    return score >= 70
+})
+newscore.forEach((ns) => {
+    console.log('New Score:'+ ns)
+ }) 
+*/
+
+
+let students = [
+    {name:'John',age:20,grade:'A'},
+    {name:'Bee',age:22,grade:'B'},
+    {name:'Mike',age:21,grade:'C'}
+]
+console.log('Student List:',students[0])
+
+let student = students.find((s)=>{
+    return(s.name === 'Mike')
+})
+
+let doubledAgeStudents = students.map((s)=>{
+    s.age = s.age * 2
+    return s
+})
+
+let highGradeStudents = students.filter((s)=>{
+    return (s.grade === 'A')
+})
+console.log('Found Student:',student)
+console.log('Doubled Age Students:',doubledAgeStudents)
+console.log('High Grade Students:',highGradeStudents)
