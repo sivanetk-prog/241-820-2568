b96f05daebd04cdedc25322a75358f330662fae6
sivanetk-prog <sivanet.k@ku.th>
2026-03-02 18:04:35 +0700

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,27 +1,45 @@
-function submitData() {
-    let firastNameDom = document.querySelector('input[name="firstName"]');
+const submitData = async () => {
+
+    let firstNameDom = document.querySelector('input[name="firstName"]');
     let lastNameDom = document.querySelector('input[name="lastName"]');
     let ageDom = document.querySelector('input[name="age"]');
     let genderDom = document.querySelector('input[name="gender"]:checked');
-    let interestDoms = document.querySelectorAll('input[name="interest"]:checked');
+    let interestDoms = document.querySelectorAll('input[name="interests"]:checked');
     let descriptionDom = document.querySelector('textarea[name="description"]');
+    let messageDom = document.getElementById('message');
 
-    let interests = ''
-    for (let i = 0; i < interestDoms.length; i++) {
-        interests += interestDoms[i].value 
-        if (i !== interestDoms.length -1) {
-            interests += ', '
+    try {
+
+        let interests = '';
+        for (let i = 0; i < interestDoms.length; i++) {
+            interests += interestDoms[i].value;
+            if (i !== interestDoms.length - 1) {
+                interests += ', ';
+            }
         }
-    }
 
-    let userData = {
-        firstName: firastNameDom.value,
-        lastName: lastNameDom.value,
-        age: ageDom.value,
-        gender: genderDom.value,
-        descriptionDom: descriptionDom.value,
-        interests: interests
+        let userData = {
+            firstName: firstNameDom.value,
+            lastName: lastNameDom.value,
+            age: ageDom.value,
+            gender: genderDom ? genderDom.value : null,
+            description: descriptionDom.value,
+            interests: interests
+        };
+
+        console.log('User Data :', userData);
+
+        const response = await axios.post('http://localhost:8000/user', userData);
+        console.log('Response :', response.data);
+
+        messageDom.innerText = "บันทึกข้อมูลสำเร็จ";
+        messageDom.className = "message success";
+
+    } catch (error) {
+
+        console.log('Error :', error.response?.data?.message || error.message);
+
+        messageDom.innerText = "เกิดข้อผิดพลาดในการบันทึกข้อมูล";
+        messageDom.className = "message error";
     }
-    console.log('submit data',userData);
-}
-    
+};
